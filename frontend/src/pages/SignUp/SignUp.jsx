403c70e49c946bf44e452e91da99ac41f215da11
JMIN <3zlll0@naver.com>
2026-08-11 20:30:33 +0900

    변수 추가

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "../../styles/AuthForm.css";
 import "../../styles/global.css";
 import { useNavigate } from "react-router-dom";
 
 function SignUp() {
     const navigate = useNavigate();
+
+    const [ formData, setFormData ] = useState({
+        nickname: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        phone: "",
+        role: "",
+    });
+    
     return (
         <div className="auth-page">
             <div className="auth-wrapper">
@@ -13,28 +24,69 @@ function SignUp() {
                 <div>
                     <div className="auth-input">
                         <p>닉네임</p>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            placeholder="vintage-market"
+                            name="nickname"
+                            value={formData.nickname}
+                        />
                     </div>
                     <div className="auth-input">
                         <p>이메일</p>
-                        <input type="text" />
+                        <input 
+                            type="text"
+                            placeholder="abcd@naver.com"
+                            name="email"
+                            value={formData.email}
+                        />
                     </div>
                     <div className="auth-input">
                         <p>비밀번호</p>
-                        <input type="password" />
+                        <input 
+                            type="password" 
+                            placeholder="비밀번호를 입력해주세요."
+                            name="password"
+                            value={formData.password}
+                        />
                     </div>
                     <div className="auth-input">
                         <p>비밀번호 확인</p>
-                        <input type="password" />
+                        <input 
+                            type="password" 
+                            placeholder="비밀번호를 다시 입력해주세요."
+                            name="passwordConfirm"
+                            value={formData.passwordConfirm}
+                        />
+                    </div>
+                    <div className="auth-input">
+                        <p>휴대폰 번호</p>
+                        <input 
+                            type="text" 
+                            placeholder="010-1234-5678"
+                            name="phone"
+                            value={formData.phone}
+                        />
                     </div>
                 </div>
                 <div className="auth-role">
                     <h4>가입유형</h4>
                     <label>
-                        <input type="radio" name="role" /><span>개인사용자</span>
+                        <input 
+                            type="radio" 
+                            name="role"
+                            value="INDIVIDUAL"
+                            checked={formData.role === "INDIVIDUAL"}
+                        />
+                        <span>개인사용자</span>
                     </label>
                     <label>
-                        <input type="radio" name="role" /><span>상점(가입 후 인증 신청 가능)</span>
+                        <input 
+                            type="radio" 
+                            name="role"
+                            value="STORE"
+                            checked={formData.role === "STORE"}
+                        />
+                        <span>상점(가입 후 인증 신청 가능)</span>
                     </label>
                 </div>
                 <div className="auth-button">
